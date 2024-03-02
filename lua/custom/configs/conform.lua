a3db13876c036f625ea646ea2a35a185f8b42162
Petar Petrov <ppetrov@pbyte.xyz>
2024-03-03 00:00:32 +0200

    custom config

---
+++ lua/custom/configs/conform.lua
@@ -0,0 +1,16 @@
+--type conform.options
+local options = {
+	lsp_fallback = true,
+
+	formatters_by_ft = {
+		lua = { "stylua" },
+
+		javascript = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+
+		sh = { "shfmt" },
+	},
+}
+
+require("conform").setup(options)
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,39 @@
+local overrides = require("custom.configs.overrides")
+
+---@type NvPluginSpec[]
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end, -- Override to setup mason-lspconfig
+  },
+
+  -- override plugin configs
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    --  for users those who want auto-save conform + lazyloading!
+    -- event = "BufWritePre"
+    config = function()
+      require "custom.configs.conform"
+    end,
+  },
+}
+
+return plugins
