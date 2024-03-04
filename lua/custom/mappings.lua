98c42e0984573445afff6df693fe0d5989936918
Petar Petrov <ppetrov@pbyte.xyz>
2024-03-04 14:13:04 +0200

    lsp format with <space>lf

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -5,12 +5,20 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
 
+    -- format with lsp
+    ["<leader>lf"] = {
+      function()
+        vim.lsp.buf.format { async = true }
+      end,
+      "Format"
+    },
+
     --  format with conform
     ["<leader>fm"] = {
       function()
         require("conform").format()
       end,
-      "formatting",
+      "Format",
     }
 
   },
